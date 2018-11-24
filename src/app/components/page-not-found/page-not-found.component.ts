95630bac7edaccf4eac881e3659a7fa00fe9afef
Robert <rrgt19@gmail.com>
2018-11-23 22:11:00 -0400

    breadcrumb fixes

--- src/app/components/page-not-found/page-not-found.component.ts
+++ src/app/components/page-not-found/page-not-found.component.ts
@@ -7,6 +7,8 @@ import {Component, OnInit} from '@angular/core';
 })
 export class PageNotFoundComponent implements OnInit {
 
+  title = 'Page Not Found';
+
   constructor() {
   }
 
--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -7,6 +7,8 @@ import {Component, OnInit} from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
+  title = 'User Profile';
+
   constructor() { }
 
   ngOnInit() {
